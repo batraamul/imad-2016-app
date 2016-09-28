3136e12fea4ac856b8893a6ff79b4a84e15a3e9e
batraamul <batraamul@gmail.com>
2016-09-28 23:48:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('AMUL', function (req, res) {
+  res.send('AMUL BATRA');
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
